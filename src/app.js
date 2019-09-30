44fdd1399ab5fac49055f57b326d3fa765a05dfa
LaurensvG <l.vangellecum@gmail.com>
2019-09-30 20:03:36 +0200

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'iPad', amount: 500}));
-store.dispatch(addExpense({ description: 'Huur auto', amount: 250}));
-store.dispatch(addExpense({ description: 'Horloge', amount: 300}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
